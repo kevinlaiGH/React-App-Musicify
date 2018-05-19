3a4a3fa05a63f186aff3e58c03609c2ffe4bc611
Kevin Lai <kevin.km.lai@gmail.com>
2018-05-19 11:03:14 +1000

    created the app component (basic wireframing)

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div className="App-title">Musicify from App</div>
+        <div>
+          <input placeholder="search an artist" />
+          <button>button</button>
+        </div>
+        <div className="Profile">
+          <div>Artist Picture</div>
+          <div>Artist Name</div>
+        </div>
+        <div className="Gallery">Gallery</div>
+      </div>
+    );
+  }
+}
+
+export default App;
